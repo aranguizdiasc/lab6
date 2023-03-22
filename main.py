7bcf63f13e038a8be6479901f8ea2bf130836008
aranguizdiasc <aranguizdiasc@ufl.edu>
2023-03-22 18:47:51 -0400

    changed main

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+if __name__ == "__main__":
+    while True:
+        # print all the menu options
+        menu()
+        # prompt the user for menu option
+        option = int(input("Please enter an option: "))
+        if option == 3:
+            break
+
