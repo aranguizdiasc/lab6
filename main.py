239aa7f8d92448decd62152acfa9aaafabf22937
aranguizdiasc <aranguizdiasc@ufl.edu>
2023-03-22 18:55:02 -0400

    changed main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 
+def menu():
+    print()
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
 
+def pass_encode(password):
+    pass
 
 if __name__ == "__main__":
     while True:
@@ -7,6 +17,10 @@ if __name__ == "__main__":
         menu()
         # prompt the user for menu option
         option = int(input("Please enter an option: "))
-        if option == 3:
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+
+            pass
+        elif option == 3:
             break
 
