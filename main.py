2272f81b5cb8cd60907e9fb46955c9e8e76f4284
aranguizdiasc <aranguizdiasc@ufl.edu>
2023-03-22 19:14:38 -0400

    changed main

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ def menu():
     print("3. Quit")
     print()
 
-def pass_encode(password):
-    pass
+def pass_encode(password): # “00009962” will become “33332295” after encoding.
+    new_pass = ""
+    for num in password[0:]:
+        new_digit = str(int(num) + 3)
+        new_pass += new_digit[-1]
+    return new_pass
 
 if __name__ == "__main__":
     while True:
@@ -17,10 +21,15 @@ if __name__ == "__main__":
         menu()
         # prompt the user for menu option
         option = int(input("Please enter an option: "))
-        if option == 1:
+        if option == 1: # encode
             password = input("Please enter your password to encode: ")
+            encoded_pass = pass_encode(password)
+            print("Your password has been encoded and stored!")
+            print(encoded_pass)
 
+        if option == 2: # decode
             pass
+
         elif option == 3:
             break
 
